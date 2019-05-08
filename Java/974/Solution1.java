4dc809f1304812c3996dbdfcf37b867c6a993469
kristyyang1110 <kristyyang1110@gmail.com>
2019-05-07 18:20:42 -0700

    add new function

---
+++ Java/974/Solution1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int subarraysDivByK(int[] A, int K) {
+        int l = A.length;
+        int t = 0;
+        int[] sum = new int[l+1];
+        for(int i = 0; i<= l; i++){
+
+        }
+        return 0;
+    }
+}
